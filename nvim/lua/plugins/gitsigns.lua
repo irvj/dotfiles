9a1fe5b009503c916e1f294f5acb81d5dfe2b3d5
Joseph Irvin <82955685+irvj@users.noreply.github.com>
2026-02-02 18:01:42 -0500

    fix pane refresh issues for git

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -5,5 +5,19 @@ return {
     opts = {
       current_line_blame = true,
     },
+    config = function(_, opts)
+      require("gitsigns").setup(opts)
+
+      vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "TermClose", "TermLeave" }, {
+        group = vim.api.nvim_create_augroup("gitsigns_refresh", { clear = true }),
+        callback = function()
+          vim.defer_fn(function()
+            if package.loaded["gitsigns"] then
+              require("gitsigns").refresh()
+            end
+          end, 100)
+        end,
+      })
+    end,
   },
 }
