35b22ebad9d6300fe95f55bf4097677a0b3bda53
Joseph Irvin <82955685+irvj@users.noreply.github.com>
2026-02-05 16:56:28 -0500

    change wsl distro

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 config.font = wezterm.font 'MesloLGS Nerd Font'
 config.font_size = 14.0
 config.color_scheme = 'Nord (Gogh)'
-config.default_prog = { 'wsl.exe', '-d', 'Ubuntu-24.04' }
+config.default_prog = { 'wsl.exe', '-d', 'jdev' }
 config.hide_tab_bar_if_only_one_tab = true
 config.window_close_confirmation = 'NeverPrompt'
 config.window_padding = { left = 8, right = 8, top = 8, bottom = 8 }
