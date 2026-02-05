ca83e461c8af9dcffea86d9317eb3809d2376af9
Joseph Irvin <82955685+irvj@users.noreply.github.com>
2026-02-04 19:38:23 -0500

    add additional wezterm settings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,6 +4,10 @@ local config = wezterm.config_builder()
 config.font = wezterm.font 'MesloLGS Nerd Font'
 config.font_size = 14.0
 config.color_scheme = 'Nord (Gogh)'
+config.default_prog = { 'wsl.exe', '-d', 'Ubuntu-24.04' }
+config.hide_tab_bar_if_only_one_tab = true
+config.window_close_confirmation = 'NeverPrompt'
+config.window_padding = { left = 8, right = 8, top = 8, bottom = 8 }
 config.front_end = 'WebGpu'
 config.webgpu_power_preference = 'HighPerformance'
 
