fa44c499a2acffc3cfe7ae6305ed7a383174df35
Joseph Irvin <82955685+irvj@users.noreply.github.com>
2026-04-23 15:11:48 -0400

    remove go from lsps

--- nvim/lua/plugins/langs.lua
+++ nvim/lua/plugins/langs.lua
@@ -5,7 +5,6 @@ return {
   { import = "lazyvim.plugins.extras.lang.php" },
   { import = "lazyvim.plugins.extras.lang.typescript" },
   { import = "lazyvim.plugins.extras.lang.python" },
-  { import = "lazyvim.plugins.extras.lang.go" },
   { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.lang.yaml" },
   { import = "lazyvim.plugins.extras.lang.markdown" },
