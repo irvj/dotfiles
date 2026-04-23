f82a3862b065042c4e7c7dc5bc9ee0a8171fb4c1
Joseph Irvin <82955685+irvj@users.noreply.github.com>
2026-04-23 14:50:31 -0400

    add lsps

---
+++ nvim/lua/plugins/langs.lua
@@ -0,0 +1,11 @@
+return {
+  { import = "lazyvim.plugins.extras.lang.rust" },
+  { import = "lazyvim.plugins.extras.lang.php" },
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.lang.go" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.yaml" },
+  { import = "lazyvim.plugins.extras.lang.markdown" },
+  { import = "lazyvim.plugins.extras.lang.docker" },
+}
