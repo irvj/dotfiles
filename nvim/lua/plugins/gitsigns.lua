f3dd6c5debfca226d9e58ca817488bdf5e067ab7
Joseph Irvin <82955685+irvj@users.noreply.github.com>
2026-04-24 16:45:31 -0400

    fix for git gutters update

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -9,12 +9,19 @@ return {
     config = function(_, opts)
       require("gitsigns").setup(opts)
 
-      vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "TermClose", "TermLeave" }, {
+      vim.api.nvim_create_autocmd("FocusGained", {
         group = vim.api.nvim_create_augroup("gitsigns_refresh", { clear = true }),
         callback = function()
           vim.defer_fn(function()
-            if package.loaded["gitsigns"] then
-              require("gitsigns").refresh()
+            if not package.loaded["gitsigns"] then
+              return
+            end
+            local gs = require("gitsigns")
+            gs.detach_all()
+            for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+              if vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buflisted then
+                pcall(gs.attach, buf)
+              end
             end
           end, 100)
         end,
